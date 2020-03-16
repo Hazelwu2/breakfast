c52bf595d7556ef45efa6b1ca76fd34112c03870
Hazel Wu <tinazx056@gmail.com>
2020-03-16 10:06:10 +0800

    [Add] 漢堡吐司蛋餅

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -20,9 +20,9 @@
           </ul>
         </div>
       </van-tab>
-      <van-tab title="單點類">
+      <van-tab title="漢吐蛋">
         <div class="box">
-          <h2>單點</h2>
+          <h2>漢堡吐司蛋餅</h2>
           <ul>
             <router-link
               v-for="(item,i) in list2"
@@ -46,6 +46,25 @@
             <router-link
               v-for="(item,i) in list3"
               :key="i + 'menu3'"
+              :to="{ name: 'Item', params: {item: i, title: item.title, subtitle: item.subtitle, desc: item.desc, price: item.price}}"
+            >
+              <li @click="saveMenu(item)">
+                <h4>{{item.title}}</h4>
+                <span>{{item.subtitle}}</span>
+                <span>{{item.desc}}</span>
+                <h6>${{item.price}}</h6>
+              </li>
+            </router-link>
+          </ul>
+        </div>
+      </van-tab>
+      <van-tab title="單點類">
+        <div class="box">
+          <h2>單點</h2>
+          <ul>
+            <router-link
+              v-for="(item,i) in list5"
+              :key="i + 'menu4'"
               :to="{ name: 'Item', params: {item: i, title: item.title, subtitle: item.subtitle, desc: item.desc, price: item.price}}"
             >
               <li @click="saveMenu(item)">
@@ -155,6 +174,137 @@ export default {
       ],
       list2: [
         {
+          title: "煎蛋",
+          subtitle: "就是煎蛋",
+          price:  20
+        },
+        {
+          title: "起司蛋",
+          subtitle: "就是起司蛋",
+          price:  30
+        },
+        {
+          title: "漢堡肉蛋",
+          subtitle: "就是漢堡肉蛋",
+          price:  30
+        },
+        {
+          title: "火腿蛋",
+          subtitle: "就是火腿蛋",
+          price:  20
+        },
+        {
+          title: "培根蛋",
+          subtitle: "就是培根蛋",
+          price:  30
+        },
+        {
+          title: "肉鬆蛋",
+          subtitle: "就是肉鬆蛋",
+          price:  30
+        },
+        {
+          title: "鮪魚蛋",
+          subtitle: "就是鮪魚蛋",
+          price:  30
+        },
+        {
+          title: "豬排蛋",
+          subtitle: "就是豬排蛋",
+          price:  40
+        },
+        {
+          title: "香雞蛋",
+          subtitle: "就是香雞蛋",
+          price:  40
+        },
+        {
+          title: "燻雞蛋",
+          subtitle: "就是燻雞蛋",
+          price:  40
+        },
+        {
+          title: "腓立蛋",
+          subtitle: "就是腓立蛋",
+          price:  40
+        },
+        {
+          title: "鮮蝦蛋",
+          subtitle: "就是鮮蝦蛋",
+          price:  40
+        },
+        {
+          title: "鱈魚蛋",
+          subtitle: "就是鱈魚蛋",
+          price:  40
+        },
+        {
+          title: "總匯",
+          subtitle: "就是總匯",
+          price:  50
+        },
+        {
+          title: "德式香腸蛋",
+          subtitle: "就是德式香腸蛋",
+          price:  45
+        },
+        {
+          title: "雞腿蛋",
+          subtitle: "就是雞腿蛋",
+          price:  50
+        },
+        {
+          title: "卡拉雞腿蛋",
+          subtitle: "就是總匯",
+          price:  50
+        },
+        {
+          title: "韓式勁辣雞腿蛋",
+          subtitle: "就是韓式勁辣雞腿蛋",
+          price:  60
+        },
+      ],
+      list3: [
+        {
+          title: "鮪魚乳酪厚片",
+          subtitle: "深海鮪魚最相思",
+          price: 50
+        },
+        {
+          title: "火腿乳酪厚片",
+          subtitle: "就是要吃加工食品",
+          price: 50
+        },
+        {
+          title: "豬排乳酪厚片",
+          subtitle: "台灣的豬超級健康的啦",
+          price: 60
+        },
+        {
+          title: "燻雞乳酪厚片",
+          subtitle: "燻雞口味",
+          price: 60
+        },
+        {
+          title: "雞腿乳酪厚片",
+          subtitle: "雞腿口味",
+          price: 65
+        }
+      ],
+      list4: [
+        {
+          title: "夏威夷披薩",
+          subtitle: "不用去夏威夷就吃得到",
+          price: 30
+        },
+        {
+          title: "總匯披薩",
+          subtitle: "要營養吃總匯",
+          price: 30
+        }
+      ],
+      list5: [
+        {
           title: "兩支熱狗",
           subtitle: "各位觀眾！兩支熱狗",
           price: 20
@@ -195,45 +345,6 @@ export default {
           price: 30
         }
       ],
-      list3: [
-        {
-          title: "鮪魚乳酪厚片",
-          subtitle: "深海鮪魚最相思",
-          price: 50
-        },
-        {
-          title: "火腿乳酪厚片",
-          subtitle: "就是要吃加工食品",
-          price: 50
-        },
-        {
-          title: "豬排乳酪厚片",
-          subtitle: "台灣的豬超級健康的啦",
-          price: 60
-        },
-        {
-          title: "燻雞乳酪厚片",
-          subtitle: "燻雞口味",
-          price: 60
-        },
-        {
-          title: "雞腿乳酪厚片",
-          subtitle: "雞腿口味",
-          price: 65
-        }
-      ],
-      list4: [
-        {
-          title: "夏威夷披薩",
-          subtitle: "不用去夏威夷就吃得到",
-          price: 30
-        },
-        {
-          title: "總匯披薩",
-          subtitle: "要營養吃總匯",
-          price: 30
-        }
-      ]
     };
   },
   methods: {
