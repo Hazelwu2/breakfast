1f72c6dbf4526405b2e652767ad4e26cf538fe79
Hazel Wu <tinazx056@gmail.com>
2020-03-18 19:20:02 +0800

    [Add] 配料、更新菜單

--- src/views/Item.vue
+++ src/views/Item.vue
@@ -9,11 +9,13 @@
     <!-- 配料 蛋餅吐司漢堡 -->
     <div class="item-subtitle text-left">
       配料要什麼呢？
-      <span><br>沒有看到你想要的？寫在備註，告訴桃子</span>
-      {{this.firstRadio}}
+      <span>
+        <br />沒有看到你想要的？寫在備註，告訴桃子
+      </span>
+  
     </div>
     <van-panel>
-      <van-radio-group @change="checkBreakfast" v-model="firstRadio">
+      <van-radio-group @change="checkAddonItem('firstRadio')" v-model="firstRadio">
         <van-radio :name="x.name" v-for="(x,i) in level(item.title)" :key="i">
           <div>
             <span class="name">{{x.name}}</span>
@@ -25,11 +27,11 @@
 
     <!-- 飲品加點 -->
     <div class="item-subtitle text-left">
-      配杯飲料吧
+      配杯飲料吧 {{addon}}
       <span>無糖飲料，請另外備註</span>
     </div>
     <van-panel>
-      <van-radio-group @change="checkAddonItem" v-model="drinkRadio">
+      <van-radio-group @change="checkAddonItem('drinkRadio')" v-model="drinkRadio">
         <van-radio :name="drink.name" v-for="(drink,i) in beverage" :key="i">
           <div>
             <span class="name">{{drink.name}}</span>
@@ -52,7 +54,7 @@
         block
         class="btn-text submit-btn"
         @click="addToCart"
-      >新增1份餐點到訂單 ${{temp}}</van-button>
+      >新增1份餐點到訂單 ${{trialPrice}}</van-button>
     </van-sticky>
   </div>
 </template>
@@ -69,35 +71,115 @@ export default {
       beverage: [
         {
           name: "溫紅茶（中）",
-          price: 10
+          price: 15
         },
         {
           name: "冰紅茶（中）",
-          price: 0
+          price: 15
         },
         {
           name: "溫紅茶（大）",
-          price: 15
+          price: 20
         },
         {
           name: "冰紅茶（大）",
-          price: 15
+          price: 20
         },
         {
           name: "溫奶茶（中）",
-          price: 5
+          price: 15
         },
         {
           name: "冰奶茶（中）",
-          price: 5
+          price: 15
         },
         {
           name: "溫豆漿（中）",
-          price: 5
+          price: 10
         },
         {
           name: "冰豆漿（中）",
-          price: 5
+          price: 10
+        },
+        {
+          name: "檸檬汁（中）",
+          price: 20
+        },
+        {
+          name: "檸檬汁（大）",
+          price: 25
+        },
+        {
+          name: "檸檬紅茶（中）",
+          price: 25
+        },
+        {
+          name: "檸檬紅茶（大）",
+          price: 25
+        },
+        {
+          name: "柳橙汁（中）",
+          price: 20
+        },
+        {
+          name: "柳橙汁（大）",
+          price: 25
+        },
+        {
+          name: "柳橙紅茶（中）",
+          price: 20
+        },
+        {
+          name: "柳橙紅茶（大）",
+          price: 25
+        },
+        {
+          name: "米漿（中）",
+          price: 10
+        },
+        {
+          name: "米漿（大）",
+          price: 20
+        },
+        {
+          name: "豆漿紅茶（中）",
+          price: 15
+        },
+        {
+          name: "豆漿紅茶（大）",
+          price: 20
+        },
+        {
+          name: "薏仁漿（中）",
+          price: 10
+        },
+        {
+          name: "薏仁漿（大）",
+          price: 20
+        },
+        {
+          name: "鮮奶茶（中）",
+          price: 30
+        },
+        {
+          name: "鮮奶茶（大）",
+          price: 40
+        },
+        {
+          name: "研磨咖啡（中）",
+          price: 30
+        },
+        {
+          name: "研磨咖啡（大）",
+          price: 40
+        },
+        {
+          name: "鮮奶咖啡（中）",
+          price: 45
+        },
+        {
+          name: "鮮奶咖啡（大）",
+          price: 55
         },
         {
           name: "不加點飲料",
@@ -207,6 +289,40 @@ export default {
         }
       ],
       level4: [
+        { // 總匯
+          name: "吐司",
+          price: 50
+        },
+        {
+          name: "漢堡",
+          price: 40
+        },
+        {
+          name: "大亨堡",
+          price: 45
+        },
+        {
+          name: "蛋餅",
+          price: 40
+        },
+        {
+          name: "捲餅",
+          price: 55
+        },
+        {
+          name: "西式燒餅",
+          price: 55
+        },
+        {
+          name: "乳酪餅",
+          price: 50
+        },
+        {
+          name: "鬆餅",
+          price: 60
+        }
+      ],
+      level5: [
         // 德式香腸蛋
         {
           name: "吐司",
@@ -241,7 +357,7 @@ export default {
           price: 60
         }
       ],
-      level5: [
+      level6: [
         {
           name: "吐司",
           price: 50
@@ -252,11 +368,11 @@ export default {
         },
         {
           name: "大亨堡",
-          price: 45
+          price: 55
         },
         {
           name: "蛋餅",
-          price: 40
+          price: 50
         },
         {
           name: "捲餅",
@@ -275,7 +391,7 @@ export default {
           price: 60
         }
       ],
-      level6: [
+      level7: [
         {
           name: "吐司",
           price: 60
@@ -309,10 +425,15 @@ export default {
           price: 70
         }
       ],
-      firstRadio: "0",
-      price: '',
-      drinkRadio: "0",
-      temp: 0,
+      firstRadio: "0", // 選蛋餅、漢堡等
+      drinkRadio: "0", // 選飲料
+      price: "", // level1-level6，不同level價格也不同
+      originPrice: 0, // 原始價格
+      trialPrice: 0, // 試算價格
+      addon: {
+        item: 0,
+        beverage: 0
+      }
     };
   },
   methods: {
@@ -320,21 +441,91 @@ export default {
       this.$router.push("/");
       this.$store.dispatch("switchShoppingCart", false);
     },
+    loop(items, name) {
+      // items 的值：飲料(beverage)、配料（item）
+      this[items].forEach(item => {
+        if (item.name == name) {
+          let temp = item.price;
+          // 勾選飲料或配料的價格
+          this.addon[items == "beverage" ? "beverage" : "item"] = item.price;
+        }
+        // 試算價格 = 原價 + 加點飲料 + 加點配料
+        this.trialPrice =
+          this.originPrice + this.addon["beverage"] + this.addon["item"];
+      });
+    },
+    checkAddonItem(radio) {
+      // 檢查加點項目是否要加價
+      // 當User change radio時會試算價格，並更新到trialPrice上
+
+      if (radio == "firstRadio") {
+        this.loop(this.price, this.firstRadio);
+      } else {
+        this.loop("beverage", this[radio]);
+      }
+    },
+    level(title) {
+      switch (title) {
+        case "煎蛋":
+          this.price = "level1";
+          return this.level1;
+        case "起司蛋":
+        case "漢堡肉蛋":
+        case "火腿蛋":
+        case "培根蛋":
+        case "肉鬆蛋":
+        case "鮪魚蛋":
+        case "玉米蛋":
+          this.price = "level2";
+          return this.level2;
+
+        case "豬排蛋":
+        case "香雞蛋":
+        case "燻雞蛋":
+        case "腓力蛋":
+        case "鮮蝦蛋":
+        case "鱈魚蛋":
+          this.price = "level3";
+          return this.level3;
+        case "總匯":
+          this.price = "level4";
+          return this.level4;
+
+        case "德式香腸蛋":
+          this.price = "level5";
+          return this.level5;
+        
+        case "雞腿蛋":
+        case "卡啦雞腿蛋":
+          this.price = "level6";
+          return this.level6;
+
+        case "韓式勁辣雞腿蛋":
+        case "牛肉蛋":
+          this.price = "level7";
+          return this.level7;
+
+        default:
+          return this.level3;
+      }
+    },
     addToCart() {
       let temp = {
         title: this.item.title,
         subtitle: this.item.subtitle || "",
         desc: this.item.desc || "",
         price: this.item.price,
-        msg: this.item.msg || ""
+        msg: this.item.msg || "",
       };
 
+      // this.price = 確定是level幾的菜單，每一層level價格都不同
       this[this.price].forEach(item => {
         if (item.name === this.firstRadio) {
           temp.title += `${item.name}`;
           temp.price += item.price;
         }
-      })
+        // temp.price = this.addon['items']
+      });
 
       this.beverage.forEach(drink => {
         if (drink.name === this.drinkRadio) {
@@ -347,69 +538,11 @@ export default {
 
       this.$store.dispatch("addToCart", temp);
       this.$toast.success("加入購物車成功");
-    },
-    loop(items, name) {
-      this[items].forEach(item => {
-        if (item.name == name) {
-          this.temp += item.price;
-        }
-      });
-    },
-    checkBreakfast() {
-      this.temp = this.item.price;
-      this.loop(this.price, this.firstRadio);
-    },
-    checkAddonItem() {
-      // 檢查加點項目是否要加價
-      this.temp = this.item.price;
-      this.loop('beverage', this.drinkRadio);
-    },
-    level(title) {
-      switch (title) {
-        case "煎蛋":
-          this.price = 'level1'
-          return this.level1;
-        case "起司蛋":
-        case "漢堡肉蛋":
-        case "火腿蛋":
-        case "培根蛋":
-        case "肉鬆蛋":
-        case "鮪魚蛋":
-
-          this.price = 'level2'
-          return this.level2;
-
-        case "豬排蛋":
-        case "香雞蛋":
-        case "燻雞蛋":
-        case "腓力蛋":
-        case "鮮蝦蛋":
-        case "鱈魚蛋":
-          this.price = 'level3'
-          return this.level3;
-
-        case "德式香腸蛋":
-          this.price = 'level4'
-          return this.level4;
-
-        case "總匯":
-        case "雞腿蛋":
-        case "卡啦雞腿蛋":
-          this.price = 'level5'
-          return this.level5;
-
-        case "韓式勁辣雞腿蛋":
-          this.price = 'level6'
-          return this.level6;
-        default:
-          return this.level1;
-      }
     }
   },
   created() {
-    this.temp = this.item.price; // 原始價格存在 temp裡
-  },
-
+    this.originPrice = this.item.price;
+  }
 };
 </script>
 
